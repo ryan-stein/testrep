6649a6239d4cbc26eba612d91f706138ae4645a4
ryan-stein <ryst8716@colorado.edu>
2025-02-18 23:57:40 -0700

    slacker Newton function (updating on constant interval

--- Labs/lab6.py
+++ Labs/lab6.py
@@ -1,6 +1,16 @@
 import numpy as np
+from numpy import random as rand
+import time
+import math
+from scipy import io, integrate, linalg, signal
+from scipy.linalg import lu_factor, lu_solve
 import matplotlib.pyplot as plt
-import sympy
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML, Video
+from mpl_toolkits.mplot3d import Axes3D
+from timeit import default_timer as timer
 
 # pre-lab: approximating derivatives
 
@@ -62,5 +72,51 @@ cd_error_1 = abs(cd_approx_1 - true_derivative(s))
 cd_p = (np.log(cd_error_1/cd_error_0))/(np.log(1/2))
 
 print(f"The order for centered difference is p = {cd_p}")
+
+
+
+
+def slackerNewton(F, J, x0, tol, Nmax):
+    """
+    Slacker Newton 
+    * x0: initial guess (NumPy array)
+    * F: vector valued function F(x)
+    * J: Jacobian matrix for F(x)
+    * tol: convergence tolerance
+    * Nmax: maximum number of iterations
+    
+    Returns: [xstar, ier, its]
+    - xstar: approximate root
+    - ier: 0 if converged, 1 if not converged after Nmax
+    - its: the iteration count at exit
+    """
+    
+    # Compute the Jacobian once at the start
+    Jx0 = J(x0)  # <-- your function for computing J(x0)
+    
+    for its in range(Nmax):
+        Fx0 = F(x0)   # <-- your function for computing F(x0)
+        
+        # Solve J(x0)*p = -F(x0) for the Newton step p
+        p = np.linalg.solve(Jx0, -Fx0)
+        
+        # Update the estimate
+        x1 = x0 + p
+
+        # Check convergence using step size
+        if np.linalg.norm(x1 - x0) < tol:
+            xstar = x1
+            ier = 0
+            return [xstar, ier, its]
+        
+        x0 = x1 
+        
+        # Recompute the Jacobian every 4th iteration 
+        if (its + 1) % 4 == 0:
+            Jx0 = J(x0)
+
+    xstar = x1
+    ier = 1
+    return [xstar, ier, its]
 
 
